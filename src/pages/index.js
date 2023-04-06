7c0dfc4796ac373d6eb1e1e13dec5bad3ace4b37
Artem Vasiliev <temav331@mail.ru>
2023-04-07 01:16:57 +0400

    9.1.1

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,20 +10,25 @@ import Section from '../components/Section.js';
 // импорт PopupWithForm
 import PopupWithForm from '../components/PopupWithForm.js';
 // импорт PopupWithImage
-import PopupWithImage from '../components/PopupWithImage';
+import PopupWithImage from '../components/PopupWithImage.js';
+// импорт PopupDelete
+import PopupDelete from '../components/PopupDelete.js';
+// импорт апи
+import Api from '../components/Api';
 // импорт переменных
 import {
     initialCards,
     configForm,
     config,
     // попапы 
-    aboutPopup, placePopup, groupPopup,
+    aboutPopup, placePopup, groupPopup, deletePopup, imagePopup,
     // инпуты 
-    workInput,nameInput,
+    workInput, nameInput,
     // ячейки для редактирования
     profileName, profileWork,
     // кнопочки
-    aboutButton, placeButton
+    aboutButton, placeButton,
+    imageProfile, imageButtonProfile
 } from '../utils/constants.js'
 // начало работы с валидацией
 const formProfile = document.querySelector('.popup__form');
@@ -34,23 +39,70 @@ formProfileValidation.enableValidation();
 // // валидация добавления места
 const formPlaceValidation = new FormValidator(configForm, formPlace);
 formPlaceValidation.enableValidation();
+// валидация изменения аватара
+const formProfileImage = document.querySelector('.popup_type_profile');
+const formProfileImageValidation = new FormValidator(configForm, formProfileImage);
+formProfileImageValidation.enableValidation();
+// работа с апи
+// Токен: 1a6e0a32-00a7-4ba0-818b-d0147d70095f
+// Идентификатор группы: cohort-62
+const api = new Api('https://mesto.nomoreparties.co/v1/cohort-62', '1a6e0a32-00a7-4ba0-818b-d0147d70095f');
+// переменная
+let currentUser;
+// 
+Promise.all([api.getCard(), api.getProfile()])
+    .then(([items, user]) => {
+        currentUser = user._id;
+        sectionAbout.renderItem(items);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 // работа с попапом редактирования 
 const infoProfile = new UserInfo(profileName, profileWork);
+// метод изменения изображения 
+function editImage(element, data) {
+    element.src = data.avatar;
+}
+// записываем значения 
+api.getProfile().then((items) => {
+    editImage(imageProfile, items);
+    // console.log(items);
+    infoProfile.setUserInfo({
+        name: items.name,
+        about: items.about,
+    })
+})
+    .catch((err) => {
+        // обработка ошибки
+        console.log(err);
+    })
 // попап редактирокания профиля
 const popupEditProfile = new PopupWithForm({
     popupSelector: aboutPopup,
     submitForm: (data) => {
-        infoProfile.setUserInfo({
-            name: data.name,
-            work: data.work,
-        });
-        popupEditProfile.close();
+        popupEditProfile.update(true);
+        api.editProfile(data)
+            .then(() => {
+                infoProfile.setUserInfo({
+                    name: data.name,
+                    about: data.about,
+                }),
+                    popupEditProfile.close();
+            })
+            .catch((err) => {
+                // обработка ошибки
+                console.log(err);
+            })
+            .finally(() => {
+                popupEditProfile.update(false);
+            })
     }
 })
 popupEditProfile.setEventListeners();
-function updateInfoProfile({name, work}){
+function updateInfoProfile({ name, about }) {
     nameInput.value = name;
-    workInput.value = work;
+    workInput.value = about;
 }
 // передача данных в попап
 aboutButton.addEventListener('click', (evt) => {
@@ -58,24 +110,70 @@ aboutButton.addEventListener('click', (evt) => {
     const dataProfile = infoProfile.getUserInfo();
     updateInfoProfile({
         name: dataProfile.name,
-        work: dataProfile.work,
+        about: dataProfile.about,
     })
-    
+    // })
     popupEditProfile.open();
 });
 // закончилась работа в попапом
 // генерация карточки
 const popupImage = new PopupWithImage(groupPopup);
 popupImage.setEventListeners();
+const popupRemove = new PopupDelete(deletePopup);
+popupRemove.setEventListeners();
 // функция генерафии карточек
 function generateCard(item) {
-    const task = new Card(config.selectorTemplatePlace, {
+    const task = new Card(config.selectorTemplatePlace, currentUser, {
         items: item,
-        handleCardClick: (name, src) => {
-            popupImage.open({ name, src });
+        handleCardClick: (name, link) => {
+            popupImage.open({ name, link });
+        },
+        handelCardDelete: () => {
+            popupRemove.open();
+            popupRemove.deleteCard(() => {
+                api.removeCard(task.getApiId())
+                    .then(() => {
+                        task.deleteCard();
+                    })
+                    .catch((err) => {
+                        // обработка ошибки
+                        console.log(err);
+                    })
+            })
+        },
+        handelCardLike: (id) => {
+            api.likeCard(id)
+                .then((result) => {
+                    // console.log(result);
+                    task.likeCard(result);
+                })
+                .catch((err) => {
+                    // обработка ошибки
+                    console.log(err);
+                })
+        },
+        handelCardDeleteLike: (id) => {
+            api.deleteLikeCard(id)
+                .then((result) => {
+                    task.likeCard(result);
+                })
+                .catch((err) => {
+                    // обработка ошибки
+                    console.log(err);
+                })
         }
     });
     return task.getElement();
+}
+// 
+// удаление карточки
+function deleteCard(id, element) {
+    console.log(id);
+    console.log(element);
+    api.removeCard(id)
+        .then(() => {
+            element.remove();
+        })
 }
 // создание и добавление
 const sectionAbout = new Section({
@@ -84,15 +182,24 @@ const sectionAbout = new Section({
         sectionAbout.addItem(generateCard(item));
     }
 },
-'.group'
+    '.group'
 );
-sectionAbout.renderItem();
 // работа с попапом места
 const popupAddCard = new PopupWithForm({
     popupSelector: placePopup,
     submitForm: (data) => {
-        sectionAbout.addItem(generateCard(data));
-        popupAddCard.close();
+        popupAddCard.update(true);
+        api.addCard(data).then((newData) => {
+            sectionAbout.addItem(generateCard(newData));
+            popupAddCard.close();
+        })
+            .catch((err) => {
+                // обработка ошибки
+                console.log(err);
+            })
+            .finally(() => {
+                popupAddCard.update(false);
+            })
     }
 })
 // обработчик открытия попапап
@@ -101,3 +208,28 @@ placeButton.addEventListener('click', (evt) => {
     popupAddCard.open();
 });
 popupAddCard.setEventListeners();
+// работа с формой
+const popupAddImage = new PopupWithForm({
+    popupSelector: imagePopup,
+    submitForm: (data) => {
+        popupAddImage.update(true);
+        api.editProfileImage(data)
+            .then((res) => {
+                console.log(res.status);
+                editImage(imageProfile, data);
+                popupAddImage.close();
+            })
+            .catch((err) => {
+                // обработка ошибки
+                console.log(err);
+            })
+            .finally(() => {
+                popupAddImage.update(false);
+            })
+    }
+})
+imageButtonProfile.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    popupAddImage.open();
+})
+popupAddImage.setEventListeners();
